682699054354e673c62aac1d64c9247d7916ee81
tancong <itancong@163.com>
2021-05-05 23:22:38 +0800

    05.05 windows

--- cloud-api-commons/src/main/java/com/atguigu/springcloud/entites/Payment.java
+++ cloud-api-commons/src/main/java/com/atguigu/springcloud/entites/Payment.java
@@ -1,14 +1,15 @@
 package com.atguigu.springcloud.entites;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Setter
+@Getter
+@ToString
 public class Payment implements Serializable {
 
     private Long id;
